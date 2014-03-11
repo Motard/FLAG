0995a0253557213bfdc79e1f0be464e141222919
Paulo Martins <pmotard@sapo-pt>
2014-03-11 10:13:47 +0000

    iMovie

---
+++ iMovie/iMovie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iMovie
+//
+//  Created by Formando FLAG on 11/03/14.
+//  Copyright (c) 2014 Paulo Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iMovie/iMovie/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iMovie
+//
+//  Created by Formando FLAG on 11/03/14.
+//  Copyright (c) 2014 Paulo Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
