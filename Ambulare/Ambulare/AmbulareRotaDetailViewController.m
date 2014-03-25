64decd8c619348ec5c9cfb1c573bea0cc5f2cd37
paulo martins <pmotard@sapo.pt>
2014-03-25 14:29:01 +0000

    MKPolyline a funcionar

--- Ambulare/Ambulare/AmbulareRotaDetailViewController.m
+++ Ambulare/Ambulare/AmbulareRotaDetailViewController.m
@@ -25,9 +25,7 @@
 {
     self.coordenadasArr =  [[NSMutableArray alloc]init];
     
-    
     int count = [self.rotasArr count];
-    
     
     //Passar as coordenadas para o NSMutableArray coordenadasArr cujo nome seja igual á rota escolhida
     for (int i = 0; i < count; i++)
@@ -48,48 +46,26 @@
     //Centrar o mapa no primeiro ponto da Rota
     CoordenadasEntity *coordenadaEntity = [self.coordenadasArr objectAtIndex:0];
     
-    
     MKCoordinateRegion mapRegion;
     mapRegion.center.latitude = [coordenadaEntity.latitude doubleValue];
     mapRegion.center.longitude = [coordenadaEntity.longitude doubleValue];
-    mapRegion.span.latitudeDelta = 0.010;
-    mapRegion.span.longitudeDelta = 0.010;
+    mapRegion.span.latitudeDelta = 0.0001;
+    mapRegion.span.longitudeDelta = 0.0001;
     self.MapView.region = mapRegion;
     
+    [self addPin];
     
-    //Colocar um marker nos pontos da rota
-  
-    for (int i=0 ; i<count ; i++)
-    {
-        CoordenadasEntity *coordenadaEntity = [self.coordenadasArr objectAtIndex:i];
-        
-        CLLocationCoordinate2D coordenada;
-        coordenada.longitude = [coordenadaEntity.longitude doubleValue];
-        coordenada.latitude = [coordenadaEntity.latitude doubleValue];
-    
-        MKPointAnnotation * point = [[MKPointAnnotation alloc]init];
-        point.coordinate = coordenada;
-        //point.title = [NSString stringWithFormat:@"%d",[coordenadaEntity.iD intValue]];
-        
-        if([coordenadaEntity.iD intValue] == 0)
-        {
-            point.title = @"Start";
-            //MKAnnotationView *pinView = [[MKAnnotationView alloc]initWithAnnotation:point reuseIdentifier:@"MyCustomAnnotation"];
-            //pinView.image = [UIImage imageNamed:@"customPin.png"];
-            //pinView.centerOffset = CGPointMake(0,0);
-        }
-        if([coordenadaEntity.iD intValue] == count-1)
-            point.title = @"End";
-        
-        [self.MapView addAnnotation:point];
-       
-    }
+    [self addRoute];
 }
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //Isto permite desenhar a MKPolyline e também criar os custom pins
+    [self.MapView setDelegate:self];
+    
 	// Do any additional setup after loading the view.
     
     AmbulareAppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
@@ -106,34 +82,105 @@
     self.rotasArr = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
 }
 
-
-
-//Customizar o MKPointAnnotation
-- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+-(void)addPin
 {
-    // If it's the user location, just return nil.
-    if ([annotation isKindOfClass:[MKUserLocation class]])
-        return nil;
     
-    // Handle any custom annotations.
-    if ([annotation isKindOfClass:[MKPointAnnotation class]])
+    int count = [self.coordenadasArr count];
+    //Colocar um marker nos pontos inicias e finais da rota
+    
+    for (int i=0 ; i<count ; i++)
     {
-        // Try to dequeue an existing pin view first.
-        MKAnnotationView *pinView = (MKAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
-        if (!pinView)
+        CoordenadasEntity *coordenadaEntity = [self.coordenadasArr objectAtIndex:i];
+        
+        CLLocationCoordinate2D coordenada;
+        coordenada.longitude = [coordenadaEntity.longitude doubleValue];
+        coordenada.latitude = [coordenadaEntity.latitude doubleValue];
+        
+        if([coordenadaEntity.iD intValue] == 0)
         {
-            // If an existing pin view was not available, create one.
-            pinView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"CustomPinAnnotationView"];
-            //pinView.animatesDrop = YES;
-            pinView.canShowCallout = YES;
-            pinView.image = [UIImage imageNamed:@"customPin.png"];
-            pinView.calloutOffset = CGPointMake(0, 32);
-        } else {
-            pinView.annotation = annotation;
+            MKPointAnnotation * point = [[MKPointAnnotation alloc]init];
+            point.coordinate = coordenada;
+            point.title = @"Start";
+            [self.MapView addAnnotation:point];
         }
-        return pinView;
+        
+        if([coordenadaEntity.iD intValue] == count-1)
+        {
+            MKPointAnnotation * point = [[MKPointAnnotation alloc]init];
+            point.coordinate = coordenada;
+            point.title = @"End";
+            [self.MapView addAnnotation:point];
+        }
+    }
+
+}
+
+
+-(void)addRoute
+{
+    CoordenadasEntity *coordenadaEntity;
+    
+    NSInteger pointsCount = [self.coordenadasArr count];
+    
+    CLLocationCoordinate2D pointsToUse[pointsCount];
+    
+    for (int i = 0; i< pointsCount ; i++)
+    {
+        for (int j = 0; j<pointsCount; j++)
+        {
+            coordenadaEntity = [self.coordenadasArr objectAtIndex:j];
+            
+            if([coordenadaEntity.iD intValue] == i)
+                break;
+        }
+        
+        pointsToUse[i] = CLLocationCoordinate2DMake([coordenadaEntity.latitude doubleValue], [coordenadaEntity.longitude doubleValue]);
+    }
+    
+    MKPolyline *myPolyline = [MKPolyline polylineWithCoordinates:pointsToUse count:pointsCount];
+    
+    [self.MapView addOverlay:myPolyline];
+    
+}
+
+-(MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id<MKOverlay>)overlay
+{
+    if ([overlay isKindOfClass:MKPolyline.class])
+    {
+        MKPolylineView *lineView = [[MKPolylineView alloc] initWithOverlay:overlay];
+        lineView.strokeColor = [UIColor blackColor];
+        return lineView;
     }
     return nil;
 }
+
+
+//Customizar o MKPointAnnotation
+//- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+//{
+//    // If it's the user location, just return nil.
+//    if ([annotation isKindOfClass:[MKUserLocation class]])
+//        return nil;
+//    
+//    // Handle any custom annotations.
+//    if ([annotation isKindOfClass:[MKPointAnnotation class]])
+//    {
+//        // Try to dequeue an existing pin view first.
+//        MKAnnotationView *pinView = (MKAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
+//        if (!pinView)
+//        {
+//            // If an existing pin view was not available, create one.
+//            pinView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"CustomPinAnnotationView"];
+//            //pinView.animatesDrop = YES;
+//            pinView.canShowCallout = YES;
+//            pinView.image = [UIImage imageNamed:@"customPin.png"];
+//            pinView.calloutOffset = CGPointMake(0, 32);
+//        } else {
+//            pinView.annotation = annotation;
+//        }
+//        return pinView;
+//    }
+//    return nil;
+//}
 
 @end
